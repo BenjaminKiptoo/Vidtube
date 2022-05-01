85543b47dc20888b5718724ccd4e06aff6e3f249
Benjamin <kiptoo925@gmail.com>
2022-05-01 10:10:03 +0300

    maincourse app with 3 thumbnails remaining

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var menuicon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+
+menu-icon.oneclick = function(){
+  sidebar.classList.toggle("small-sidebar");
+}
